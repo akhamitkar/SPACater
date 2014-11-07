d4abb9a32df3f96604642198074d3d093dc5c5a4
saasbook <saasbook@saasbook.(none)>
2014-11-06 21:12:46 -0500

    Making it a single page application

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('caterorder', []);
+
+app.controller('OrderJsonCtrl', [ '$http',function($http){
+ var jsonorders = this;
+ jsonorders.orders = [];
+ $http.get('/orders.json').success(function(data){
+  jsonorders.orders = data;
+ });
+ }]);
