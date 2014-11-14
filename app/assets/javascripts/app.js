f1b8e10fc5625c904ddd7d72d5fc07d7d9fff58b
saasbook <saasbook@saasbook.(none)>
2014-11-14 00:25:45 -0500

    Add for the SPA now works

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,43 @@
 var app = angular.module('caterorder', []);
 
-app.controller('OrderJsonCtrl', [ '$http',function($http){
- var jsonorders = this;
- jsonorders.orders = [];
- $http.get('/orders.json').success(function(data){
-  jsonorders.orders = data;
- });
- }]);
+app.controller('DeptController', ['$http',function($http) {
+  var caterorder = this;
+   caterorder.depts = [];
+  $http.get('/depts.json').success(function(data){
+    caterorder.depts=data;  
+	}); // http get close
+
+
+  this.dept ={}; // Dept init  
+  this.addDept = function(dept){  // addDept simply calls the post
+  $http.post('/depts.json',{dept}); // Next line is interesting.
+  $http.get('/depts.json').success(function(data){ // We load depts again to reflect row add 
+    caterorder.depts=data;  
+   });
+  }; // addDept close
+ 
+  // Start get a dept
+
+
+
+ }]); // Controller close
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
